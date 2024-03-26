1dfa970445e40d4b367b46615d0e52db918d800c
PHANINDRA KUMAR NIDAMANURI <git config --global user.email>
2024-03-26 22:46:50 +0530

    headings decreations colors and bg

---
+++ theory/theory.js
@@ -0,0 +1,12 @@
+//Bootstrap 5 is CSS framework
+
+
+//Bootstrap 5 New Features
+
+//Bootstrap 5 has dropped jQuery
+//Forms have been revamped
+//Right-To-Left support
+//New utility classes(positioning,font size,boarder radius etc)
+//Minor changes to some components  and grid (jumbotron gone)
+//Bootstrap icons
+//Two new components-Offcanvas and Accordion 
